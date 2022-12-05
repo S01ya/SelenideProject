ebe4cac747966ff358b5941899a82f95073f338d
Olga Sokolinskaya <olga_genplan@hotmail.com>
2022-12-05 03:19:49 +0300

    Testing

--- src/test/java/com/it_academy/onliner/ProductsFromCategoryTest.java
+++ src/test/java/com/it_academy/onliner/ProductsFromCategoryTest.java
@@ -1,18 +1,22 @@
 package com.it_academy.onliner;
+
 import com.it_academy.onliner.pageobject.OnlinerHeader;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import jdk.jfr.Description;
 import org.testng.annotations.Test;
 
-public class ProductsFromCategoryTest extends BaseTest{
-        @Test
-        @Description("This test verifies that section 'Accessories'  from category 'Computers " +
-                "and nets' contains elements with names, price, and amount of goods")
-    public void testElementsOfCategoryAccessories (){
+public class ProductsFromCategoryTest extends BaseTest {
+    @Test
+    @Description("This test verifies that section 'Accessories'  from category 'Computers " +
+            "and nets' contains elements with names, price, and amount of goods")
+    @Severity(SeverityLevel.CRITICAL)
+    public void testElementsOfCategoryAccessories() {
         new OnlinerHeader().navigateToOnliner()
                 .clickOnCatalog()
                 .clickOnCategory("Компьютеры")
-            .clickOnCatalogClassifierLink(" Комплектующие ")
+                .clickOnCatalogClassifierLink(" Комплектующие ")
                 .collectProductsFromCategory();
-        }
+    }
 }
 
