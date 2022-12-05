2718254ea40ccee203e0e508741147d285dbb37d
Olga Sokolinskaya <olga_genplan@hotmail.com>
2022-12-05 12:59:48 +0300

    Testing

--- src/main/java/com/it_academy/onliner/framework/OperaDriverCreator.java
+++ src/main/java/com/it_academy/onliner/framework/OperaDriverCreator.java
@@ -1,13 +1,13 @@
 package com.it_academy.onliner.framework;
-//import io.github.bonigarcia.wdm.managers.OperaDriverManager;
 
+import io.github.bonigarcia.wdm.managers.OperaDriverManager;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class OperaDriverCreator implements WebDriverCreator<RemoteWebDriver> {
     @Override
     public RemoteWebDriver create() {
-        // OperaDriverManager.operadriver().setup();
+        OperaDriverManager.operadriver().setup();
         return new OperaDriver();
     }
 }
